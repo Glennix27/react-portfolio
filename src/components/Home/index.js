9f95d31c0f755fcca044cb658c3d133a8e547699
Glennix <92360411+Glennix27@users.noreply.github.com>
2023-09-25 03:44:52 +0700

    initial commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import Photo from './Photo/binus.png'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray1 = ['G', 'l', 'e', 'n', 'n', 'i', 'x']
+  const nameArray2 = [
+    'X',
+    'a',
+    'v',
+    'e',
+    'r',
+    'i',
+    'u',
+    's',
+    '',
+    'M',
+    'a',
+    'r',
+    'l',
+    'o',
+    'w',
+  ]
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray1}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray2}
+              idx={15}
+            />
+            <br />
+          </h1>
+          <h2>Undergraduate from Bina Nusantara University</h2>
+          <Link to="/about" className="flat-button">
+            ABOUT ME
+          </Link>
+        </div>
+        <div className="image-container">
+          <img src={Photo} alt="Photo" className="image-class" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,70 @@
+import './index.scss'
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faBars } from '@fortawesome/free-solid-svg-icons'
+import { NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
+
+  return (
+    <div className="nav-bar">
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/glennix-xaverius-marlow-132b76291/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Glennix27"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#00ffc8"
+        size="3x"
+        className="hamburger-icon"
+      />
+    </div>
+  )
+}
+
+export default Sidebar
